d30ebbac646a3df72e815b40d572eb833544308c
Nishanth Goud <nishanthgoud@dynamic-10-125-255-23.frisco.wireless.unt.edu>
2022-10-16 22:26:21 -0500

    added new function

--- gfgEasy.java
+++ gfgEasy.java
@@ -37,11 +37,32 @@ public class gfgEasy {
         sum+=Math.abs(a[0]-a[i]);
         return sum;
     }
+    public static Boolean isMultipleof3(int n){
+        if(n==0)
+            return true;
+        if(n<0)
+            n=~n;
+        if(n==1)
+            return false;
+        int odd_count=0;
+        int even_count=0;
+        while(n!=0){
+            if((n&1)!=0){
+                odd_count++;
+            }
+            if((n&2)!=0){
+                even_count++;
+            }
+            n=n>>2;
+        }
+        return isMultipleof3(even_count-odd_count);
+    }
     public static void main(String[] args) {
 //        long[] a={-3,2,-2,2};
 //        gfgEasy g=new gfgEasy();
 //        g.Arrange(a,4);
 //        print(a);
-        System.out.println(swapAndMax(new int[] {4,2,1,8}));
+       // System.out.println(swapAndMax(new int[] {4,2,1,8}));
+        System.out.println(isMultipleof3(23));
     }
 }
