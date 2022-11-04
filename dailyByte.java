8c9890338b393b9749da2033e05618b2b50bffb1
Nishanth Goud <nishanthgoud@dynamic-10-125-214-84.dynamic.unt.edu>
2022-11-04 10:04:48 -0500

    day-3

---
+++ dailyByte.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+import java.util.Locale;
+
+public class dailyByte {
+    //This question is asked by Google. Given a string, reverse all of its characters and return the resulting string.
+    //first i would like to execute the following problem using brute force
+    public static void rev(String s){
+        char[] a=s.toCharArray();
+        int j=s.length()-1;
+        for(int i=0;i<j;i++,j--){
+            char temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+        }
+        for(char i:a){
+            System.out.print(i);
+        }
+
+    }
+    //reversing individual letters in a string
+    public static void in(String s){
+        String[] str=s.split("\\s");
+        String sbc="";
+        for(String q:str){
+            StringBuilder sb=new StringBuilder(q);
+            sb.reverse();
+            sbc+=sb.toString()+" ";
+        }
+        System.out.println(sbc);
+    }
+    //Valid palindrome or not
+    public static boolean check(String s){
+        //first we are going to use the functions of stringbulider
+        StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
+        String rev=sb.toString();
+        if(s.equals(rev))
+            return true;
+        return false;
+    }
+    //Vaccum clearner Route
+    public static boolean route(String s){
+        char[] c=s.toCharArray();
+        int cl=0,cr=0,cu=0,cd=0;
+        for(int i=0;i<c.length;i++){
+            if(c[i]=='l')
+                cl++;
+            else if(c[i]=='r')
+                cr++;
+            else if(c[i]=='u')
+                cu++;
+            else
+                cd++;
+        }
+        if(cl-cr==0&&cu-cd==0)
+            return true;
+        return false;
+    }
+    //Correct Capitalization
+    public static boolean correction(String s){
+        String str=s;
+        String sC=str.toUpperCase();
+        String sL=str.toLowerCase();
+        String cam=str.substring(0,1).toUpperCase()+s.substring(1);
+        if(s.equals(sC)||s.equals(sL)||s.equals(cam))
+            return true;
+        return false;
+    }
+    //Binary String
+    public static Byte Binary(Byte b){
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(correction("coding"));
+//        String s="ruulldrd";
+//        System.out.println(route(s));
+//        String s="North Texas";
+//        System.out.println(s);
+//        rev(s);
+//        System.out.println();
+//        in(s);
+//        String s="levelsr";
+//        System.out.println(check(s));
+    }
+}
