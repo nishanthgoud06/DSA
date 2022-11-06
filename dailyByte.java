ef110fda57029f74b084d1ade1dd7387f851438d
Nishanth Goud <nishanthgoud@dynamic-10-125-214-84.frisco.wireless.unt.edu>
2022-11-06 00:14:05 -0500

    day-4

--- dailyByte.java
+++ dailyByte.java
@@ -67,11 +67,121 @@ public class dailyByte {
         return false;
     }
     //Binary String
-    public static Byte Binary(Byte b){
-        
+    public static String Binary(String s1,String s2){
+        StringBuilder sb=new StringBuilder();
+       int i=s1.length()-1;
+       int j=s2.length()-1;
+       int carry=0;
+       while(i>=0 ||j>=0){
+           int sum=carry;
+           if(i>=0)sum+=s1.charAt(i--)-'0';
+           if(j>=0)sum+=s2.charAt(j--)-'0';
+           sb.append(sum%2);
+           carry=sum/2;
+       }
+       if(carry!=0)
+           sb.append(carry);
+       return sb.reverse().toString();
     }
+//    Longest Common Prefix
+    public static String Prefix(String[] arr){
+        //the approach which we are going to implement is comparing word by word
+        if(arr.length ==0)
+            return "";
+        if(arr.length==1)
+            return arr[0];
+        Arrays.sort(arr);
+        int min=Math.min(arr[0].length(),arr[arr.length-1].length());
+        int i=0;
+        while(i<min&&arr[0].charAt(i)==arr[arr.length-1].charAt(i))
+            i++;
+        return arr[0].substring(0,i);
+    }
+    //the same program, but now we are iterating through the all string but comparing the character
+    public static String Prefix1(String[] array){
+        int min=findmin(array);
+        String result="";
+        char a;
+        for(int i=0;i<min;i++){
+             a=array[0].charAt(i);
+             for(int j=1;j<array.length;j++){
+                 if(a!=array[j].charAt(i))
+                     return result;
+             }
+        result+=a;
+        }
+        return result;
+    }
+    public static int findmin(String[] array){
+        int min=array[0].length();
+        for(int i=1;i<array.length;i++){
+            min=Math.min(min,array[i].length());
+        }
+        return min;
+    }
+    //finding the longest prefix using divide and conquer method
+    public static String DaC(String[] array,int low,int high){
+        if(low==high)
+            return array[low];
+        if(high>low){
+            int mid=low+(high-low)/2;
+            String str1=DaC(array,low,mid);
+            String str2=DaC(array,mid+1,high);
+            return (doit(str1,str2));
+        }
+        return null;
+    }
+    public static String doit(String s1,String s2){
+        String result="";
+        int a=s1.length();
+        int b=s2.length();
+        for(int i=0,j=0;i<=a-1&&j<=b-1;i++,j++){
+            if(s1.charAt(i)!=s2.charAt(j))
+                break;
+            result+=s1.charAt(i);
+        }
+        return result;
+    }
+    //Remove a character from a string to make it a palindrome
+    public static int palind(String str){
+        int low=0,high=str.length()-1;
+        while(low<high){
+            if(str.charAt(low)==str.charAt(high)){
+                low++;
+                high--;
+            }
+            if(palcheck(str,low+1,high))
+                return low;
+            if(palcheck(str,low,high-1))
+                return high;
+            return -1;
+        }
+        return -2;
+    }
+    public static boolean palcheck(String str,int low,int high){
+        while(low<high){
+            if(str.charAt(low)!=str.charAt(high))
+                return false;
+        low++;
+        high--;
+        }
+        return true;
+    }
+    
     public static void main(String[] args) {
-        System.out.println(correction("coding"));
+//        String s1="foobof";
+//        int a=palind(s1);
+//        if(a==-1)
+//            System.out.println("its not a palindrome");
+//        else if(a==-2)
+//            System.out.println("its a palindrome");
+//        else
+//            System.out.println("its a palindrome by removing a character which is at "+ a);
+//        System.out.println(DaC(new String[] {"spot", "spotty", "spotted"},0,2));
+//        System.out.println(Prefix1(new String[] {"spot", "spotty", "spotted"}));
+//        System.out.println(Prefix(new String[] {"spot", "spotty", "spotted"}));
+//        System.out.println(Binary("11","11"));
+//        System.out.println(correction("coding"));
 //        String s="ruulldrd";
 //        System.out.println(route(s));
 //        String s="North Texas";
