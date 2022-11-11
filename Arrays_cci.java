e93f65ee66a0204803b8fde4a4351afb3c985b8d
Nishanth Goud <nishanthgoud@dynamic-10-125-244-21.frisco.wireless.unt.edu>
2022-11-11 10:04:27 -0600

    day-8

---
+++ Arrays_cci.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Arrays_cci {
+    //1.1 implement anAlgorithm to determine if a string has all unique character.
+    // what i you cannot use additional data Structure
+    public static boolean isUnique(String s){
+        //but the time complexity is O(n2)
+        char[] c=s.toCharArray();
+        for(int i=0;i<c.length;i++){
+            for(int j=i+1;j<c.length;j++){
+                if(c[i]==c[j])
+                    return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isUniques(String s){
+        char[] c=s.toCharArray();
+        Arrays.sort(c);
+        for(int i=0;i<c.length-1;i++){
+            if(c[i]==c[i+1])
+                return false;
+        }
+        return true;
+    }
+    //check permutations
+    public static boolean permutations(String s1,String s2){
+        if(s2.length()==0)
+            return false;
+        return checkPermutations(s1,s2);
+    }
+    public static boolean checkPermutations(String s1,String s2){
+        String[] a=allp(s2);
+    }
+    static List<String> coll=new ArrayList<>();
+    public static String[] allp(String s1,int low,int high){
+        if(low==high)
+            coll.add()
+
+    }
+    public static void main(String[] args) {
+        System.out.println(isUnique("cabc"));
+    }
+}
