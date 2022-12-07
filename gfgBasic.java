3b8527459d514a0a47ebc857e46fc7d86869a76b
Nishanth Goud <nishanthgoud@dynamic-10-125-189-178.frisco.wireless.unt.edu>
2022-12-07 04:08:35 -0600

    complete daily leetcode count

--- gfgBasic.java
+++ gfgBasic.java
@@ -1,161 +1,215 @@
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
 
 public class gfgBasic {
-    //this class includes all the basic level form gfg practise section
-    //Cyclically rotate an array by one
-    public static void rotateCy(int[] a){
-        int temp=a[a.length-1];
-        for(int i=a.length-1;i>=1;i--){
-            a[i]=a[i-1];
-        }
-        a[0]=temp;
-    }
-    //First and last occurrences of x
-    //Given a sorted array arr containing n elements with possibly duplicate elements,
-    // the task is to find indexes of first and last occurrences of an element x in the given array
-    public static ArrayList<Integer> flO(int[] a,int target){
-        int apperance=0;
-        int  first=-1;
-        int last =-1;
-        for(int i=0;i<a.length;i++){
-            if(a[i]==target && apperance==0){
-                first=i;
-                apperance=1;
-            }
-            if(a[i]==target){
-                last=i;
-            }
-        }
-        ArrayList<Integer> arrayList=new ArrayList<>(2);
-        arrayList.add(first);
-        arrayList.add(last);
-        return arrayList;
-    }
-    //for this program we are implementing stacks using array
-    //stacks perform last in first out.
-    class g{
-        int top=-1;
-        int[] arr;
-        public g(){
-            arr=new int[100];
-        }
-        public  void push(int i){
-            arr[++top]=i;
-        }
-        public int pop(){
-            if(top==-1)
-                return -1;
-            else
-                return arr[top--];
-        }
-    }
-    class a{
-        int front,rear=0;
-        int[] arr;
-        public a(){
-            arr=new int[100];
-        }
-        void push(int i){
-            arr[rear++]=i;
-        }
-        int pop(){
-            if(rear-front>0){
-                return arr[front++];
-            }
-            return -1;
-        }
-    }
-    public static void sortit(int[][] arr){
-        int a= arr.length;
-        int b=arr[0].length;
-        ArrayList<Integer> ab=new ArrayList<>();
-        int k=0;
-        for(int i=0;i<a;i++){
-            for(int j=0;j<b;j++){
-                ab.add(arr[i][j]);
-            }
-        }
-        Collections.sort(ab);
-        for(int i=0;i<a;i++){
-            for(int j=0;j<b;j++){
-                arr[i][j]=ab.get(k++);
-            }
-        }
-    }
-    public static void printing(int[][] a){
-        for(int i=0;i<a.length;i++){
-            for(int j=0;j<a[0].length;j++){
-                System.out.println(a[i][j]);
-            }
-        }
-    }
-    //Bit Difference
-    public static int bitDiff(int a,int b){
-        int c=a^b;
-        return Integer.bitCount(c);
-    }
-    //to check whether the enter Node is a circular linked list or not
-    static class Node{
+//    //this class includes all the basic level form gfg practise section
+//    //Cyclically rotate an array by one
+//    public static void rotateCy(int[] a){
+//        int temp=a[a.length-1];
+//        for(int i=a.length-1;i>=1;i--){
+//            a[i]=a[i-1];
+//        }
+//        a[0]=temp;
+//    }
+//    //First and last occurrences of x
+//    //Given a sorted array arr containing n elements with possibly duplicate elements,
+//    // the task is to find indexes of first and last occurrences of an element x in the given array
+//    public static ArrayList<Integer> flO(int[] a,int target){
+//        int apperance=0;
+//        int  first=-1;
+//        int last =-1;
+//        for(int i=0;i<a.length;i++){
+//            if(a[i]==target && apperance==0){
+//                first=i;
+//                apperance=1;
+//            }
+//            if(a[i]==target){
+//                last=i;
+//            }
+//        }
+//        ArrayList<Integer> arrayList=new ArrayList<>(2);
+//        arrayList.add(first);
+//        arrayList.add(last);
+//        return arrayList;
+//    }
+//    //for this program we are implementing stacks using array
+//    //stacks perform last in first out.
+//    static class g{
+//        int top=-1;
+//        int[] arr;
+//        public g(){
+//            arr=new int[100];
+//        }
+//        public  void push(int i){
+//            arr[++top]=i;
+//        }
+//        public int pop(){
+//            if(top==-1)
+//                return -1;
+//            else
+//                return arr[top--];
+//        }
+//    }
+//    static class a{
+//        int front,rear=0;
+//        int[] arr;
+//        public a(){
+//            arr=new int[100];
+//        }
+//        void push(int i){
+//            arr[rear++]=i;
+//        }
+//        int pop(){
+//            if(rear-front>0){
+//                return arr[front++];
+//            }
+//            return -1;
+//        }
+//    }
+//    public static void sortit(int[][] arr){
+//        int a= arr.length;
+//        int b=arr[0].length;
+//        ArrayList<Integer> ab=new ArrayList<>();
+//        int k=0;
+//        for(int i=0;i<a;i++){
+//            for(int j=0;j<b;j++){
+//                ab.add(arr[i][j]);
+//            }
+//        }
+//        Collections.sort(ab);
+//        for(int i=0;i<a;i++){
+//            for(int j=0;j<b;j++){
+//                arr[i][j]=ab.get(k++);
+//            }
+//        }
+//    }
+//    public static void printing(int[][] a){
+//        for(int i=0;i<a.length;i++){
+//            for(int j=0;j<a[0].length;j++){
+//                System.out.println(a[i][j]);
+//            }
+//        }
+//    }
+//    //Bit Difference
+//    public static int bitDiff(int a,int b){
+//        int c=a^b;
+//        return Integer.bitCount(c);
+//    }
+//    //to check whether the enter Node is a circular linked list or not
+//    static class Node{
+//        int val;
+//        Node next;
+//        Node(int val){
+//            this.val=val;
+//            next=null;
+//        }
+//    }
+//    //to check whether a binary search tree is balanced or not
+//    //to check wetaher the node is Circular or not
+//    public static boolean checlCir(Node test){
+//        Node temp=test;
+//        while(temp.next!=null){
+//            temp=temp.next;
+//        }
+//        if(temp.next==test)
+//            return true;
+//        return false;
+//    }
+//    public static int middle(int a,int b,int c){
+//        int result=Math.max(a,Math.max(b,c));
+//        if(result==a)
+//            result=Math.max(b,c);
+//        else if(result==b)
+//            result=Math.max(a,c);
+//        else
+//            result=Math.max(a,b);
+//        return result;
+//    }
+//    public static int median(int[] a){
+//        Arrays.sort(a);
+//        int mid= (int) Math.floor(a.length/2);
+//        return ((mid&1)==1)?a[mid]:(a[mid]+a[mid-1])/2;
+//    }
+//    //Find the Duplicate Number
+//    //Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+//    //
+//    //There is only one repeated number in nums, return this repeated number.
+//    //
+//    //You must solve the problem without modifying the array nums and uses only constant extra space.
+//    public static  int Dupli(int[] a){
+//        int slow=a[0];
+//        int fast=a[0];
+//        do{
+//            slow=a[slow];
+//            fast=a[a[fast]];
+//        }while(slow!=fast);
+//        fast=a[0];
+//        while(slow!=fast){
+//            slow=a[slow];
+//            fast=a[fast];
+//        }
+//        return slow;
+//    }
+//to check whether a given tree is balanced or not
+    static class node{
         int val;
-        Node next;
-        Node(int val){
+        node left,right;
+        public node(int val){
             this.val=val;
-            next=null;
+            this.left=null;
+            this.right=null;
         }
+}
+    static boolean result=true;
+    public static int perform(node n){
+        if(n==null)
+            return 0;
+        int left=perform(n.left);
+        int right=perform(n.right);
+        if(Math.abs(left-right)>1)
+           result=false;
+        return 1+Math.max(left,right);
     }
-    //to check whether a binary search tree is balanced or not
-    //to check wetaher the node is Circular or not
-    public static boolean checlCir(Node test){
-        Node temp=test;
-        while(temp.next!=null){
-            temp=temp.next;
-        }
-        if(temp.next==test)
-            return true;
-        return false;
-    }
-    public static int middle(int a,int b,int c){
-        int result=Math.max(a,Math.max(b,c));
-        if(result==a)
-            result=Math.max(b,c);
-        else if(result==b)
-            result=Math.max(a,c);
-        else
-            result=Math.max(a,b);
+    public static boolean isBal(node n){
+
+        perform(n);
         return result;
     }
-    public static int median(int[] a){
-        Arrays.sort(a);
-        int mid= (int) Math.floor(a.length/2);
-        return ((mid&1)==1)?a[mid]:(a[mid]+a[mid-1])/2;
-    }
-    //Find the Duplicate Number
-    //Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
-    //
-    //There is only one repeated number in nums, return this repeated number.
-    //
-    //You must solve the problem without modifying the array nums and uses only constant extra space.
-    public static  int Dupli(int[] a){
-        int slow=a[0];
-        int fast=a[0];
-        do{
-            slow=a[slow];
-            fast=a[a[fast]];
-        }while(slow!=fast);
-        fast=a[0];
-        while(slow!=fast){
-            slow=a[slow];
-            fast=a[fast];
+    //to check whether the two node are same or not
+    public static boolean similar(node n1,node n2){
+        Queue<node> queue=new LinkedList<>();
+        if(n1==null && n2==null)
+            return true;
+        if(n1==null||n2==null)
+            return false;
+        if(n1!=null&& n2!=null){
+            queue.offer(n1);
+            queue.offer(n2);
         }
-        return slow;
+        while(!queue.isEmpty()){
+            node first= queue.poll();
+            node second= queue.poll();
+            if (first == null && second == null)
+                continue;
+            if (first == null || second == null)
+                return false;
+            if (first.val != second.val)
+                return false;
+            queue.offer(first.left);
+            queue.offer(second.left);
+            queue.offer(first.right);
+            queue.offer(second.right);
+        }
+        return true;
     }
-
     public static void main(String[] args) {
-        System.out.println(Dupli(new int[]{1,2,3,4,5,1}));
+        node n=new node(3);
+        n.left=new node(9);
+        n.right=new node(20);
+        n.right.right=new node(1);
+        n.right.right.right=new node(5);
+//        System.out.println(isBal(n));
+//        System.out.println(Dupli(new int[]{1,2,3,4,5,1}));
 //        System.out.println(median(new int[] {56,67,30,79}));
 //        System.out.println(middle(23,45,65));
 //        Node head=null;
