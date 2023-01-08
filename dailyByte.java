e4753ff0afb698a435c9a9f19163b6540821e254
Nishanth Goud <nishanthgoud@Nishanths-Air.lan>
2023-01-07 23:48:54 -0600

    updated hard question

--- dailyByte.java
+++ dailyByte.java
@@ -1758,7 +1758,25 @@ public static boolean canCross(int[] stones) {
         }
         return max;
     }
-
+    //72. Edit Distance
+    public static int minDistance(String word1, String word2) {
+        int[][] dp = new int[word2.length()+1][word1.length()+1];
+        for(int i = 0; i <= word2.length(); i++) {
+            for(int j = 0; j <= word1.length(); j++) {
+                if(i == 0 && j == 0) dp[i][j] = 0;
+                else if(i == 0 && j != 0) {
+                    dp[i][j] = j;
+                } else if(i != 0 && j == 0) {
+                    dp[i][j] = i;
+                } else if(word2.charAt(i-1) != word1.charAt(j-1)) {
+                    dp[i][j] = Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1])) + 1;
+                }else {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+            }
+        }
+        return dp[word2.length()][word1.length()];
+    }
     public static void main(String[] args) {
 //        System.out.println(maxSub(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
         System.out.println(longCS("abcde","ace"));
